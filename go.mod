efcbd2931f337cda260c713460fdbab1d15c8348
bootjp / Yoshiaki Ueda <contact@bootjp.me>
2022-10-22 23:12:43 +0900

    first version

---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/bootjp/linode-firewall-apply
+
+go 1.19
+
+require (
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/linode/linodego v1.9.3
+	golang.org/x/oauth2 v0.1.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/go-resty/resty/v2 v2.1.1-0.20191201195748-d7b97669fe48 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	golang.org/x/net v0.1.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"github.com/linode/linodego"
+	"net"
+
+	"golang.org/x/oauth2"
+	"strconv"
+
+	"log"
+	"net/http"
+	"os"
+)
+
+func init() {
+	var ok bool
+	var err error
+	apiKey, ok = os.LookupEnv("LINODE_TOKEN")
+	if !ok {
+		log.Fatal("Could not find LINODE_TOKEN, please assert it is set.")
+	}
+	sfid, ok := os.LookupEnv("FIREWALL_ID")
+	if !ok {
+		log.Fatal("Could not find FIREWALL_ID, please assert it is set.")
+	}
+	fid, err = strconv.Atoi(sfid)
+	if err != nil {
+		log.Fatal("Could not convert FIREWALL_ID to int")
+	}
+	label, ok = os.LookupEnv("FIREWALL_RULE_LABEL")
+	if !ok {
+		log.Fatal("Could not find FIREWALL_RULE_LABEL, please assert it is set.")
+	}
+
+	redisHost, ok = os.LookupEnv("REDIS_HOST")
+	if !ok {
+		log.Fatal("Could not find REDIS_HOST, please assert it is set.")
+	}
+
+	redisPort, ok = os.LookupEnv("REDIS_PORT")
+	if !ok {
+		log.Fatal("Could not find REDIS_PORT, please assert it is set.")
+	}
+
+	redisPassword, ok = os.LookupEnv("REDIS_PASSWORD")
+	if !ok {
+		log.Println("Redis Password is not set, assuming no password")
+	}
+
+	redisKey, ok = os.LookupEnv("REDIS_KEY")
+	if !ok {
+		log.Fatal("Could not find REDIS_PORT, please assert it is set.")
+	}
+}
+
+var label string
+var fid int
+var apiKey string
+var redisPort string
+var redisHost string
+var redisPassword string
+var redisKey string
+
+func main() {
+	ips, err := getAllowIps(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ips) == 0 {
+		log.Fatal("No IPs found in redis")
+	}
+
+	fmt.Println("apply ips")
+	fmt.Println(ips)
+
+	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: apiKey})
+	oauth2Client := &http.Client{
+		Transport: &oauth2.Transport{
+			Source: tokenSource,
+		},
+	}
+
+	linodeClient := linodego.NewClient(oauth2Client)
+	res, err := linodeClient.GetFirewall(context.Background(), fid)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, rule := range res.Rules.Inbound {
+		if rule.Label == label {
+			*rule.Addresses.IPv4 = ips
+		}
+	}
+
+	for _, rule := range res.Rules.Inbound {
+		if rule.Label == label {
+			fmt.Printf("%+v %+v", rule, rule.Addresses.IPv4)
+		}
+	}
+	_, err = linodeClient.UpdateFirewallRules(context.Background(), fid, res.Rules)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func getAllowIps(ctx context.Context) ([]string, error) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     net.JoinHostPort(redisHost, redisPort),
+		Password: redisPassword,
+		DB:       0,
+	})
+
+	ips, err := rdb.SMembers(ctx, redisKey).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	return ips, nil
+}
